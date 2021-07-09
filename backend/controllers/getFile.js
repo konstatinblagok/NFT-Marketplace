987315990a61102b4aa440c438ca55d2a8dd8c48
espsofttech <rahuliitm69@gmail.com>
2021-07-09 15:36:03 +0530

    New Code Update

--- backend/controllers/getFile.js
+++
@@ -1,8 +0,0 @@
-
-const path = require('path')
-exports.getImage = async (req, res) => {
-    const image = req.params.image;
-    const myPath = path.resolve(process.cwd(), "uploads", image);
-    res.sendFile(myPath);
-}
-
